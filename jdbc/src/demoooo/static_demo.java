55c6163c919e4373354040bd60547c402860a12e
Pranaypalav <pranaypalav18@gmail.com>
2021-07-05 20:12:10 +0530

    static demo

---
+++ jdbc/src/demoooo/static_demo.java
@@ -0,0 +1,36 @@
+package demoooo;
+class stats{
+	
+	// static variable
+	static int  num2=display1();
+	static String name="sudarshan";
+	static int num=99;
+	
+	
+	// static method
+	static void display(){
+		System.out.println("static method calling");
+		
+	}
+	static int display1(){
+		System.out.println("hello static ");
+		return 6;
+		
+	}
+}
+public class static_demo extends stats {
+static int num1;
+
+//  static block
+	static{
+		num1=num;  // u  Cannot make a static reference to the non-static field num1(if u not declare num1 as a static)
+	}
+	public static void main(String args[]){
+		display();     // u can call static method without initialize the object
+	System.out.println(name);
+	System.out.println(num1);
+	System.out.println(num2);
+	}
+	
+
+}
