f0dd65b836b7296dc0fb1294514d5b689855b980
Pranaypalav <pranaypalav18@gmail.com>
2021-07-06 20:54:00 +0530

    push

---
+++ jdbc/src/demoooo/public_access_modifier.java
@@ -0,0 +1,13 @@
+package demoooo;
+import oops.*;  // import the package
+                // main2 and aggrigation2 is useful from oops package for this programme
+
+public class public_access_modifier extends aggrigation2 {
+	public static void main(String args[]){
+		public_access_modifier pm=new public_access_modifier();
+		main2 m1=new main2(73, 22);
+		System.out.println(pm.display1(m1));
+		
+	}
+
+}
