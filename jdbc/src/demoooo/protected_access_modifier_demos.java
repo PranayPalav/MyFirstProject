84f186be441b931d040985ebffcfe4d1fe20d5ed
PranayPalav <86422306+PranayPalav@users.noreply.github.com>
2021-07-07 19:15:51 +0530

    Update protected_access_modifier_demos.java

--- jdbc/src/demoooo/protected_access_modifier_demos.java
+++ jdbc/src/demoooo/protected_access_modifier_demos.java
@@ -4,6 +4,22 @@ import oops.*;
 
 // if u need to access protected data from another package its compulsory to import the package...
 // and inheritance is compulsory....
+
+//  error code 
+
+//public class protected_access_modifier_demos{
+//	public static void main(String args[]){
+//		protected_access_modifier_demos pd=new protected_access_modifier_demos();
+//		constructordemo pd=new constructordemo();
+//		pd.run();
+//		//access protected data
+//		System.out.println("access protected variable j:- "+pd.j);
+//		
+//	}
+//
+//}
+
+
 public class protected_access_modifier_demos extends constructordemo {
 	public static void main(String args[]){
 		protected_access_modifier_demos pd=new protected_access_modifier_demos();
